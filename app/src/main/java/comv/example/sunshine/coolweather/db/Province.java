203f428987f7f360ffed392b07329487328013dd
Sunshine <1577628182@qq.com>
2018-10-19 09:04:16 +0800

    加入创建数据库和表的各项配置。“

---
+++ app/src/main/java/comv/example/sunshine/coolweather/db/Province.java
@@ -0,0 +1,37 @@
+package comv.example.sunshine.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Sunshine on 2018/10/19.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
